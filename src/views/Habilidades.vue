4d25619aacd3ee7f7c08bfabde7577cb51f6c949
Carlos Alberto <carlos.neurozze@gmail.com>
2023-01-17 15:17:00 -0300

    Projeto de uma pokedex

---
+++ src/views/Habilidades.vue
@@ -0,0 +1,65 @@
+<template>
+    <div>
+        <div v-if="!pokemon.habilidades">
+            Selecione um Pokémon
+        </div>
+        <div v-else>
+            <table class="table text-white">
+                <tbody>
+                    <transition-group name="lista">
+                        <tr v-for="(h, indice) in habilidadesOrdenadas" :key="h">
+                            <td>{{ h }}</td>
+                            <td class="d-flex justify-content-end">
+                                <button 
+                                    type="button" 
+                                    class="btn btn-danger btn-sm"
+                                    @click="$emit('removerHabilidade', indice)"
+                                >
+                                    x
+                                </button>
+                            </td>
+                        </tr>
+                    </transition-group>
+                </tbody>
+            </table>
+            <input 
+                type="text"
+                class="form-control"
+                placeholder="Adicionar habilidade"
+                v-model="habilidade"
+                @keyup.enter="adicionarHabilidade"
+            >
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Habilidades',
+    props: {
+        pokemon: Object
+    },
+    data: () => ({
+        habilidade: ''
+    }),
+    methods: {
+        adicionarHabilidade() {
+            //console.log(this.habilidade)
+            this.$emit('adicionarHabilidade', this.habilidade)
+            this.habilidade = ''
+        }
+    },
+    computed: {
+        habilidadesOrdenadas() {
+            let habilidades = this.pokemon.habilidades
+            return habilidades.sort()
+        }
+    }
+}
+</script>
+
+<style scoped>
+.table td {
+    border: none;
+}
+</style>
